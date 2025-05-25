960ab46f54e4698abc0cd818424748d54ad3c5bd
Roland von Ohlen <roland@rvo-host.net>
2025-05-25 16:36:40 +0200

    feat: Make colors less ugly

--- config.py
+++ config.py
@@ -2,6 +2,12 @@ from dataclasses import dataclass
 from enum import Enum
 
 
+COLOR_GREEN = [64, 192, 87, 255]  # RGBA
+COLOR_RED = [250, 82, 82, 255]  # RGBA
+COLOR_ORANGE = [253, 126, 20, 255]  # RGBA
+COLOR_YELLOW = [250, 186, 5, 255]  # RGBA
+
+
 class ServiceStatus(Enum):
     """Enum for service status."""
 
@@ -27,33 +33,21 @@ class ServiceStatusInfo:
 
 STATUS_MAP = {
     ServiceStatus.STARTING: ServiceStatusInfo(
-        label="Starting",
-        icon="server-bolt.svg",
-        background_color=[255, 165, 0, 255],  # Orange
+        label="Starting", icon="server-bolt.svg", background_color=COLOR_ORANGE
     ),
     ServiceStatus.RUNNING: ServiceStatusInfo(
-        label="Running",
-        icon="server.svg",
-        background_color=[0, 255, 0, 255],  # Green
+        label="Running", icon="server.svg", background_color=COLOR_GREEN
     ),
     ServiceStatus.STOPPING: ServiceStatusInfo(
-        label="Stopping",
-        icon="server-bolt.svg",
-        background_color=[255, 165, 0, 255],  # Orange
+        label="Stopping", icon="server-bolt.svg", background_color=COLOR_ORANGE
     ),
     ServiceStatus.STOPPED: ServiceStatusInfo(
-        label="Stopped",
-        icon="server-off.svg",
-        background_color=[255, 0, 0, 255],  # Red
+        label="Stopped", icon="server-off.svg", background_color=COLOR_RED
     ),
     ServiceStatus.ERROR: ServiceStatusInfo(
-        label="Error",
-        icon="plug-x.svg",
-        background_color=[255, 0, 0, 255],  # Red
+        label="Error", icon="plug-x.svg", background_color=COLOR_RED
     ),
     ServiceStatus.PARTIAL: ServiceStatusInfo(
-        label="Partial",
-        icon="server-bolt.svg",
-        background_color=[255, 255, 0, 255],  # Yellow
+        label="Partial", icon="server-bolt.svg", background_color=COLOR_YELLOW
     ),
 }
